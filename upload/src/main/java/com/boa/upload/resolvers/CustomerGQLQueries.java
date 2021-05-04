49ba8b60978072b152edb0b1116adfaf3804c5cc
eswaribala <parameswaribala@gmail.com>
2021-05-04 11:29:32 +0530

    v3

--- upload/src/main/java/com/boa/upload/resolvers/CustomerGQLQueries.java
+++ upload/src/main/java/com/boa/upload/resolvers/CustomerGQLQueries.java
@@ -3,11 +3,13 @@ package com.boa.upload.resolvers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.boa.upload.models.Customer;
 import com.boa.upload.services.CustomerService;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 
+@Component
 public class CustomerGQLQueries implements GraphQLQueryResolver {
 	@Autowired
 	private CustomerService customerService;
