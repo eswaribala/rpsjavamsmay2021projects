59aad372d0d9e97de01321349f7b3e3e755145f9
eswaribala <parameswaribala@gmail.com>
2021-05-06 10:11:56 +0530

    v6

--- uploadspringcloud/src/main/java/com/boa/upload/UploadApplication.java
+++ uploadspringcloud/src/main/java/com/boa/upload/UploadApplication.java
@@ -1,9 +1,13 @@
 package com.boa.upload;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+
 public class UploadApplication {
 
 	public static void main(String[] args) {
